216d3754d22aff1ce14c0783a2ed02d6a0246e93
Tianjun Mo <motianjun4@gmail.com>
2022-01-30 22:37:06 -0500

    add: branch coverage

--- runcov.sh
+++ runcov.sh
@@ -3,9 +3,9 @@ make all
 cat testcases.txt |grep cyoa | xargs -L 1 -I {} bash -c "./{}" 
 gcov -b page.cpp path.cpp story.cpp cyoa-step1.cpp cyoa-step2.cpp cyoa-step3.cpp cyoa-step4.cpp> covresult.txt
 
-lcov -d . -c -o covinfo
-lcov --extract covinfo "$PWD/*" -o covinfo_filtered
-genhtml -o docs covinfo_filtered
+lcov --rc lcov_branch_coverage=1 -d . -c -o covinfo
+lcov --rc lcov_branch_coverage=1 --extract covinfo "$PWD/*" -o covinfo_filtered
+genhtml --branch-coverage -o docs covinfo_filtered
 echo ""
 echo "---------------------------"
 echo "coverage result in covresult.txt"
